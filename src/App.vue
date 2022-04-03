1bf698a070c04de414c7f4511ef39efbe12db645
Eyüp Duran <eduedue.577@gmail.com>
2022-04-03 13:33:46 +0300

    Vue giriş

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+  <ProductAdd @add:product="addProduct"/>
+  <ProductList  @delete:product="deleteProduct" @update:product="updateProduct" :products="products" />
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProductList from './components/ProductList.vue'
+import ProductAdd from './components/ProductAdd.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ProductList,
+    ProductAdd
+  },
+  data(){
+    return{
+      products:[
+        {id:1,categoryId:1,productName:'Laptop',quantityPerUnit:'Acer Laptop',UnitPrice:5000,unitsInStock:2},
+        {id:2,categoryId:1,productName:'Mouse',quantityPerUnit:'Acer Maouse',UnitPrice:50,unitsInStock:3},
+        {id:3,categoryId:2,productName:'Keyboard',quantityPerUnit:'Acer Keyboard',UnitPrice:500,unitsInStock:20}
+      ]
+    }
+  },
+  methods:{
+    deleteProduct(product){
+      this.products = this.products.filter(
+        productToFilter => productToFilter.id!==product.id
+      );
+    },
+     updateProduct(){
+      
+    },
+    addProduct(product){
+      const newProduct ={...product}
+      this.products=[...this.products,newProduct]
+    }
   }
 }
 </script>
@@ -19,7 +46,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/ProductAdd.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="addForm">
+      <form @submit.prevent="handleAdd">
+          <div class="form-group">
+              <label for="id">Id</label>
+              <input v-model="product.id" type="text" class="form-control" id="id">
+          </div>
+          <div class="form-group">
+              <label for="categoryId">Category</label>
+              <input v-model="product.categoryId" type="text" class="form-control" id="categoryId">
+          </div>
+          <div class="form-group">
+              <label for="productName">Ürün adı</label>
+              <input v-model="product.productName" type="text" class="form-control" id="productName">
+          </div>
+          <div class="form-group">
+              <label for="quantityPerUnit">Açıklama</label>
+              <input v-model="product.quantityPerUnit" type="text" class="form-control" id="quantityPerUnit">
+          </div>
+          <div class="form-group">
+              <label for="UnitPrice">Birim fiyatı</label>
+              <input v-model="product.UnitPrice" type="text" class="form-control" id="UnitPrice">
+          </div>
+          <div class="form-group">
+              <label for="unitsInStock">Stok Adedi</label>
+              <input v-model="product.unitsInStock" type="text" class="form-control" id="unitsInStock">
+          </div>
+          <button type="submit" class="btn btn-sm btn-success">Ekle</button>
+      </form>  
+  </div>
+ 
+</template>
+
+<script>
+export default {
+    name:"product-add",
+    data(){
+        return{
+            product:{
+            id:"",categoryId:"",productName:"",quantityPerUnit:"",UnitPrice:"",unitsInStock:""
+            }
+        }
+    },
+    methods:{
+        handleAdd(){
+            this.$emit("add:product",this.product)
+        }
+    }
+}
+</script>
+
+<style scoped>
+#addForm{
+    margin: 100px;
+}
+</style>
